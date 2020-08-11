4e709129d5378c39ae3db60e7600cd7f608172e8
rsurpur20 <42478922+rsurpur20@users.noreply.github.com>
2020-08-10 23:25:28 -0400

    finished iteration 0

---
+++ ToDoListProject/ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Roshni Surpur on 8/10/20.
+//  Copyright © 2020 Roshni Surpur. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+
+   var description = ""
+   var important = false
+
+}
